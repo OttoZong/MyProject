f6f92b92aae98f705b0eb786b5e1589a8a571e94
OttoZong <37859051+OttoZong@users.noreply.github.com>
2018-12-15 18:14:42 +0800

    Add files via upload

---
+++ CHAT_Server/src/main/java/v0/Server.java
@@ -0,0 +1,83 @@
+package v0;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+/**
+ * 聊天室服務端
+ * @author Otto
+ *
+ */
+public class Server {
+	/*
+	 * java.net.ServerSocket
+	 * 運行在server端的ServerSocket
+	 * 
+	 * 主要作用有兩個:
+	 * 1.向系統申請服務端口,客戶端就是通過這個端口與之連結的.
+	 * 2.監聽該服務端口,一旦客戶端通過這個端口請求連結,
+	 * 	  則創建一個Socket與該客戶端進行通訊.
+	 */
+	private ServerSocket server;
+	
+	public Server() {
+		try {
+			/*
+			  * 實例化ServerSocket的同時申請服務端口.
+			  * 若該端口已經被其他程序占用,則會拋出異常.
+			 * address already in use
+			 */
+			System.out.println("請啟動服務端...");
+			server = new ServerSocket(8088);
+//			System.out.println("構造方法：server:"+server);
+			System.out.println("服務端啟動完畢!");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void start() {
+		try {
+			/*
+			 * ServerSocket提供的方法:
+			 * Socket accept()
+			 * 	該方法是一個阻塞方法,調用到該方法後程序"卡住",
+			 * 	並等待客戶端的連接,一旦一個客戶端連結了,
+			 * 	那麼就會返回一個Socket實例,通過該實例即可與
+			 * 	連接的客戶端進行通訊.
+			 */
+			while(true) {
+				System.out.println("等待客戶端連接....");
+	//			System.out.println("server:"+server);
+				Socket socket = server.accept();
+	//			System.out.println("socket:"+socket);
+				System.out.println("一個客戶端連結了!");
+			}
+//			InputStream in = socket.getInputStream();
+//			InputStreamReader isr = new InputStreamReader(in,"UTF-8");
+//			BufferedReader br = new BufferedReader(isr);
+//			
+//			while(true) {
+//				String line = br.readLine();
+//				System.out.println("客戶端說:"+line);
+//			}
+			
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	public static void main(String[] args) {
+		Server server = new Server();
+		server.start();
+	}
+	/*
+	 * 如何查看IP,Window、Linux
+	  * 補充：
+	 * Window, CMD介面 , ipconfig
+	 * Linux, 控制台, sbin/ifconfig
+	 */
+}
---
+++ CHAT_Server/src/main/java/v4/Client.java
@@ -0,0 +1,108 @@
+package v4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+/**
+ * 聊天客戶端
+ * @author Otto
+ */
+
+public class Client {
+	/*
+	 * java.net.Socket
+	 * 封裝了TCP通訊協定,中文翻譯是:套頭字,
+	 * 使用Socket的大致步驟:
+	 * 1.實例化Socket,同時指定連結的服務端的IP
+	 * 和端口並與服務端建立連接.
+	 * 2.通過Socket創造兩個流,一個輸入流一個輸出流
+	 * 通過輸入流讀取遠端計算機發送過來的數據,
+	 * 通過輸出流將數據發送給對方計算機
+	 */
+	private Socket socket;
+	/**
+	 * 用於初始化客戶端
+	 */
+	public Client() {
+		try {
+			/*
+			 * 實例化Socket時需要傳入兩個參數
+			 * 1.IP位址,通過IP可以找到網絡上的指定計算機.
+			 * 2.端口,用來連接該計算機上的對應應用程序.
+			 * 
+			 * 實例化Socket就是與服務端連接的過程
+			 */
+			System.out.println("正在連結服務端....");
+			socket = new Socket("localhost",8088);
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.out.println("與服務端建立連結!");
+		}
+	}
+	
+	/*
+	 * 程序的啟動方法
+	 */
+	public void start() {
+		try {
+			//啟動讀取服務端消息的線程
+			ServerHandler handler = new ServerHandler();
+			Thread t = new Thread(handler);
+			t.start();
+			
+			OutputStream out = socket.getOutputStream();
+			OutputStreamWriter osw = new OutputStreamWriter(out,"UTF-8");
+			PrintWriter pw = new PrintWriter(osw,true);		//true = 自動行刷新.
+			
+			
+			System.out.println("請開始輸入內容: ");
+			Scanner scan = new Scanner(System.in);
+			String message = null;
+			
+			long lastSend = System.currentTimeMillis()-1000;
+			while(true) {
+				String line = scan.nextLine();
+				//說話必須間隔一秒鐘
+				if(System.currentTimeMillis()-lastSend>=1000) {
+					pw.println(line);
+					System.out.println("寫出完畢!");
+				}else {
+					System.out.println("您說話速度太快!");
+				}
+				lastSend = System.currentTimeMillis();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	public static void main(String[] args) {
+		Client client = new Client();
+		client.start();
+		 
+	}
+	/*
+	 * 該線程用於讀取服務端發送過來的消息,並輸出控制台
+	 */
+	private class ServerHandler implements Runnable{
+		public void run() {
+			try {
+				BufferedReader br = new BufferedReader(
+						new InputStreamReader(socket.getInputStream(),"UTF-8")
+				);
+				String line = null;
+				while((line=br.readLine())!=null) {
+					System.out.println(line);
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
